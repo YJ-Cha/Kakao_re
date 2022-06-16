8f60412028d6e8aeb994d5295afbd3a91d0cfae3
YJ-Cha <107171334+YJ-Cha@users.noreply.github.com>
2022-06-16 16:39:26 +0900

    재시도2

---
+++ README.md
@@ -0,0 +1,5 @@
+연습용 사이트입니다.# Kakao
+# Kakao2
+# Kakao2
+# Kakao2
+# Kakao_re
---
+++ js/main.js
@@ -0,0 +1,25 @@
+$('.topLeft').click(function(){
+    $('.gnb_open').animate({left:0},300);
+    $('.dark').fadeIn(300);
+});
+
+$('.dark').click(function(){
+    $('.dark').fadeOut(200);
+    $('.gnb_open').animate({left: '-400px'},200);
+    $('.loginPop').hide();
+});
+
+$('.btn_charactor').click(function(){
+    $(this).find('span').toggleClass('on');
+    $('.characterWrap').slideToggle(300);
+});
+
+$('.btn_category').click(function(){
+    $(this).find('span').toggleClass('on');
+    $('.categoryWrap').slideToggle(300);
+});
+
+$('.log').click(function(){
+    $('.gnb_open').animate({left: '-400px'},200);
+    $('.loginPop').show();
+})
